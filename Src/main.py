ebbe33e315e485ac15b18478045061ec87bafcad
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-08 22:26:38 +0330

    Added new methods

--- Src/main.py
+++ Src/main.py
@@ -1,4 +1,20 @@
+import random
+
 MAX_LINES=3
+MAX_BET=100
+MIN_BET=1
+ROWS =3
+COLS=3
+
+symbol_count={
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8,
+}
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols =[]
+    
 def Deposit():
     while True:
         amount=input("What would you like to deposit? $")
@@ -26,10 +42,29 @@ def get_number_of_lines():
             print("PLease enter a number")
 
     return lines
- 
+
+def get_bet():
+    while True:
+        amount=input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount=int(amount)
+            if MIN_BET<=amount<=MAX_BET:
+                break
+            else:
+                print(f" must be greater between. ${MAX_BET} - ${MAX_BET}")
+        else:
+            print("PLease enter a number")
+    return amount
 def main():
     balance= Deposit()
     lines= get_number_of_lines()
-    print(balance,lines)
+    bet= get_bet()
+    total_bet=bet*lines
+    if total_bet>balance:
+        print(f"You don't have enough to bet that amount, your current balance is : ${balance}")
+    else:
+        break
+
+    print(f"you are betting ${bet} on ${lines} lines. total bet is equal to ${total_bet}")
 
 main()
